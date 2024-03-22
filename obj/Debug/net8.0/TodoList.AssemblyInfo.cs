7af54ee714e58c4d900edfca7e115b71209b205d
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-03-22 18:40:09 -0500

    Add lib

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab1a6d5d4660d82d85a653288933b7e5fdcbbdab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
