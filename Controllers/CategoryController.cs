e703fa78683245d8365c855327e3088f952e39a7
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-03-27 16:25:49 -0500

    Add create for category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -13,9 +13,32 @@ public class CategoryController : Controller
     _context = context;
   }
 
+  [HttpGet]
   public async Task<IActionResult> Index()
   {
     List<Category> categories = await _context.Categories.ToListAsync(); 
     return View(categories);
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Create()
+  {
+    List<Category> parents = await _context.Categories.Where(c => c.ParentId == null).ToListAsync();
+    ViewBag.Parents = parents;
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Create(Category category)
+  {
+    if (category == null) 
+    {
+      return NotFound();
+    }
+
+    _context.Categories.Add(category);
+    await _context.SaveChangesAsync();
+
+    return RedirectToAction(nameof(Index));
   }
 }
