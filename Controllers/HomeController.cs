b307c3d107dd413829c1ac5807b630982596ce9c
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-03-26 20:09:54 -0500

    Add task

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,117 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TodoList.Models;
+using TodoList.Models.ViewModel;
 
 namespace TodoList.Controllers;
+
+/*
+  TODO
+  * Agregar validaciones, ViewModelHome
+*/
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        return View();
+      var tasks = await _context.Tasks.Include(c => c.Category).ToListAsync();
+      return View(tasks);
     }
 
-    public IActionResult Privacy()
+    [HttpGet]
+    public async Task<IActionResult> Create()
     {
-        return View();
+      ViewBag.Categories = await _context.Categories.ToListAsync();
+      return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    [HttpPost]
+    public async Task<IActionResult> Create(ViewModelTask model)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      if (ModelState.IsValid) {
+        var task = new TodoList.Models.Task
+        {
+          Id = model.Id,
+          CategoryId = model.CategoryId,
+          Title = model.Title,
+          Description = model.Description,
+          StartDate = model.StartDate,
+          EndDate = model.EndDate
+        };
+
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
+      }
+
+      return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    [Route("tarea/editar/{id}")]
+    public async Task<IActionResult> Edit(uint id)
+    {
+      var task = await _context.Tasks.FindAsync(id);
+
+      if (task == null) {
+        Response.StatusCode = 404;
+        return NotFound();
+      }
+
+      var viewModel = new ViewModelTask{
+        Title = task.Title,
+        CategoryId = task.CategoryId,
+        Description = task.Description,
+        StartDate = task.StartDate,
+        EndDate = task.EndDate
+      };
+
+      ViewData["Categories"] = await _context.Categories.ToListAsync();
+
+      return View(viewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(uint id, ViewModelTask model) {
+      if (ModelState.IsValid) {
+        var task = new TodoList.Models.Task(){
+          Id = model.Id,
+          CategoryId = model.CategoryId,
+          Title = model.Title,
+          Description = model.Description,
+          StartDate = model.StartDate,
+          EndDate = model.EndDate
+        };
+
+        _context.Tasks.Update(task);
+        await _context.SaveChangesAsync();
+      }
+
+      return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(uint? Id)
+    {
+      if (Id == null) {
+        return NotFound();
+      }
+
+      var task = await _context.Tasks.FindAsync(Id);
+      _context.Tasks.Remove(task);
+      await _context.SaveChangesAsync();
+
+      return RedirectToAction(nameof(Index));
     }
 }
---
+++ Models/ViewModel/ViewModelTask.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace TodoList.Models.ViewModel;
+
+public class ViewModelTask
+{
+    public uint Id { get; set; }
+
+    [Required(ErrorMessage = "Es necesario elegir una categoria")]
+    [Display(Name = "Categoria")]
+    public uint CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Escriba su nombre.")]
+    [MinLength(4, ErrorMessage = "Escriba al menos 5 caracteres.")]
+    [MaxLength(50, ErrorMessage = "Escriba un máximo de 50 caracteres.")]
+    [Display(Name = "Título")]
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? StartDate { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime? EndDate { get; set; }
+}
